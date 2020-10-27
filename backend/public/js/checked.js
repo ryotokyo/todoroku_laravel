bf649c72f4618c92762a4c00bbf7918d949cd8dd
ryotokyo <ryoymnk.banbi@gmail.com>
2020-10-27 22:46:53 +0900

    機能完成

---
+++ backend/public/js/checked.js
@@ -0,0 +1,17 @@
+
+// is_completedが1の場合は、完了済みで表示する
+//
+// $(function(){
+//
+//   var checkedId = $(".todoTable__check").attr('id')
+//   var finishFlag = $("#checkedId").val();
+//   console.log($(".todoTable__check").attr('id'));
+//
+//   if(finishFlag === '1' ){
+//     $(".todoTable__check").prop("checked", true);
+//     $(".todoTable__check").closest("tr").css("text-decoration", "line-through");
+//   }
+//
+// });
+
+
